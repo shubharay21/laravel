8bc9025fa5b0092eda212e68c31e94edd96b220d
shubharay21 <shubharay21@gmail.com>
2025-12-05 16:08:12 +0530

    feat:wip

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,33 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        // return($request->all());
         $credentials = $request->only('email', 'password');
-        if (!$token = auth()->attempt($credentials)) {
+        if (!$token = Auth::attempt($credentials)) {
             return response()->json(['error' => 'Invalid Credentials'], 401);
         }
-
         return response()->json([
             'token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => Auth::factory()->getTTL() * 60
         ]);
     }
 
     public function me()
     {
-        return response()->json(auth()->user());
+        return response()->json(Auth::user());
     }
 
     public function logout()
     {
-        auth()->logout();
+        Auth::logout();
 
         return response()->json(['message' => 'Logged out successfully']);
     }
@@ -37,7 +35,7 @@ class AuthController extends Controller
     public function refresh()
     {
         return response()->json([
-            'token' => auth()->refresh()
+            'token' => Auth::refresh()
         ]);
     }
 }
