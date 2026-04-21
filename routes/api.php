a238848b1131116c0e5268971d87ba1bdbe3c91d
Shubhadeep Ray <shubharay21@gmail.com>
2026-04-21 12:42:37 +0530

    wip

--- routes/api.php
+++ routes/api.php
@@ -418,6 +418,86 @@ Route::post('/test', function (Request $request) {
             return response()->json(json_encode($responseData), 200);
             break;
         case '1068':
+            $responseData = [
+                "RemoteIP" => null,
+                "ApplicationId" => -999,
+                "TriggeredByUserId" => "",
+                "ActionId" => 0,
+                "ActionMethodName" => "",
+                "ActionNameSpace" => "",
+                "GroupMethodName" => "",
+                "GroupNameSpace" => "",
+                "NoOfArguments" => 0,
+                "RequestType" => "",
+                "MethodArg" => [],
+                "MethodArgLite" => [],
+                "DbTuple" => [],
+                "SqlScriptList" => [],
+                "Base64ObjectString" => "",
+                "DeviceTypeId" => 0,
+                "DeviceId" => "",
+                "DbTupleLite" => [
+                    [
+                        "TableName" => "Dummy_Response_Table",
+                        "PrimaryKeyField" => "",
+                        "PrimaryKeyDbField" => "",
+                        "DbName" => "",
+                        "RecordList" => [
+                            [
+                                "Record" => [
+                                    [
+                                        "Fn" => "lotNumber",
+                                        "Fv" => "F303202603928055",
+                                        "Dt" => ""
+                                    ],
+                                    [
+                                        "Fn" => "totalRecord",
+                                        "Fv" => "40",
+                                        "Dt" => ""
+                                    ],
+                                    [
+                                        "Fn" => "successCount",
+                                        "Fv" => "29",
+                                        "Dt" => ""
+                                    ],
+                                    [
+                                        "Fn" => "rejectedCount",
+                                        "Fv" => "9",
+                                        "Dt" => ""
+                                    ],
+                                    [
+                                        "Fn" => "status",
+                                        "Fv" => "Partial",
+                                        "Dt" => ""
+                                    ]
+                                ],
+                                "DbTupleLite" => null
+                            ]
+                        ]
+                    ]
+                ],
+                "ApiTrailId" => -999,
+                "TransactionId" => 0,
+                "Rrn" => "",
+                "ExtRefNo" => "",
+                "Base64Objects" => [],
+                "IsActionBlocked" => false,
+                "ActionName" => "",
+                "StoredProcArg" => [
+                    "StoredProcName" => "",
+                    "ArgumentListLite" => [],
+                    "ReturnField" => ["Fn" => "", "Fv" => "", "Dt" => ""],
+                    "DbServerId" => "",
+                    "DefaultDBName" => ""
+                ],
+                "ResponseStatus" => "SUCCESS",
+                "ErrorMessage" => "",
+                "ErrorDetail" => "",
+                "ErrorCode" => "",
+                "ErrorLocation" => "",
+                "ExceptionLogId" => ""
+            ];
+            return response()->json(json_encode($responseData), 200);
             break;
         case '1073':
             $responseData = [
