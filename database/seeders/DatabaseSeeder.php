cc088f17cbccb6354a3f6637a7828a6ac16c9774
Shubhadeep Ray <shubharay21@gmail.com>
2026-04-28 14:23:50 +0530

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'password' => Hash::make('password'),
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\UserTable;
 use App\Http\Controllers\LBController;
 Route::get('/', function () {
+// return '<h1>Hello</h1>';
     return view('online');
 });
 
@@ -13,4 +14,10 @@ Route::controller(LBController::class)->group(function () {
     Route::any('/sendtolb', 'sendtolb')->name('sendtolb');
     Route::any('/logoutfromlb', 'logoutfromlb')->name('logoutfromlb');
     Route::any('/refreshtokenforlb', 'refreshtokenforlb')->name('refreshtokenforlb');
+});
+
+
+Route::get('testView', function () {
+// return '<h1>Hello</h1>';
+    return view('testView');
 });
